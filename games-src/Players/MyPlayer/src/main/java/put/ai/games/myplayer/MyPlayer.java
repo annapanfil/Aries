1f5037c1d3754c1b15f5cc822f5fe946ca1d7099
panka134 <134panka@gmail.com>
2022-01-22 14:52:09 +0100

    Negmax long thinking

--- games-src/Players/MyPlayer/src/main/java/put/ai/games/myplayer/MyPlayer.java
+++ games-src/Players/MyPlayer/src/main/java/put/ai/games/myplayer/MyPlayer.java
@@ -1,11 +1,13 @@
 package put.ai.games.myplayer;
 
+import org.graalvm.compiler.core.common.type.ArithmeticOpTable;
 import put.ai.games.aries.AriesBoard;
 import put.ai.games.aries.AriesMove;
 import put.ai.games.game.Board;
 import put.ai.games.game.Move;
 import put.ai.games.game.Player;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import static java.lang.Math.min;
@@ -13,6 +15,7 @@ import static java.lang.Math.min;
 public class MyPlayer extends Player {
 
   boolean debug = true;
+  int MAX_DEPTH = 2;
 
   @Override
   public String getName() {
@@ -21,15 +24,24 @@ public class MyPlayer extends Player {
 
   private int getDistanceToWinningField(int x, int y, Color player, Board b){
     if (player == Color.PLAYER1)
-      return 2 * b.getSize() - x - y; // win in bottom right corner
+      return 2 * (b.getSize()-1) - x - y; // win in bottom right corner
     else
       return x+y; // win on 0,0
   }
 
-  private int stateEvaluation(Board b){
-    /* Calculate objective function value */
+  int checkWinning(Board b, Color player){
+      if (b.getState(0,0) == Color.PLAYER2)
+          if (player == Color.PLAYER2) return Integer.MAX_VALUE;
+          else return Integer.MIN_VALUE+1;
+      else if (b.getState(b.getSize(), b.getSize()) == Color.PLAYER1)
+          if (player == Color.PLAYER1) return Integer.MAX_VALUE;
+          else return Integer.MIN_VALUE+1;
+      else return 0;
+  }
 
-    Color player = getColor();
+  private int stateEvaluation(Board b, Color player){
+    /* Calculate objective function value – to maximize*/
+
     Color opponent = getOpponent(player);
     AriesBoard board = (AriesBoard) b;
 
@@ -44,24 +56,24 @@ public class MyPlayer extends Player {
               my_pieces++;
               my_distance = min(getDistanceToWinningField(i, j, player, board), my_distance);
               if (my_distance == 0)
-                  return Integer.MIN_VALUE; //our winning move
+                  return Integer.MAX_VALUE; //our winning move
           }
           else if (b.getState(i, j) == opponent){
               opponents_pieces++;
               opponents_distance = min(getDistanceToWinningField(i, j, opponent, board), opponents_distance);
 
               if (opponents_distance == 0)
-                  return Integer.MAX_VALUE; //their winning move
+                  return Integer.MIN_VALUE+1; //their winning move
           }
       }
     }
 
-      if (debug){
-        System.out.println("Pieces: My " + my_pieces + "Their "+ opponents_pieces);
-        System.out.println("Distance: My " + my_distance + "Their " + opponents_distance);
-      }
+//      if (debug){
+//        System.out.println("Pieces: " + player + " " + my_pieces +" "+ opponent + " "+ opponents_pieces);
+//        System.out.println("Distance: " + player + " " +  my_distance + " " + opponent + " " + opponents_distance);
+//      }
 
-      return 9*opponents_pieces + 10*my_distance - 8*my_pieces - 7*opponents_distance;
+      return -9*opponents_pieces - 10*my_distance + 8*my_pieces + 7*opponents_distance; // my_pieces - my_distance - opponents_pieces + opponents_distance;
   }
 
 
@@ -81,14 +93,12 @@ public class MyPlayer extends Player {
           boardNew = board.clone();
           boardNew.doMove(move);
 
+          curr_move_value = stateEvaluation(boardNew, getColor());
+//          if (debug){
+//              System.out.println(move.toString() + " " + curr_move_value);
+//          }
 
-
-          curr_move_value = stateEvaluation(boardNew);
-          if (debug){
-              System.out.println(move.toString() + " " + curr_move_value);
-          }
-
-          if (curr_move_value == Integer.MIN_VALUE) // winning move
+          if (curr_move_value == Integer.MIN_VALUE+1) // winning move
               return curr_move;
           if (curr_move_value < best_move_value) {
               best_move = curr_move;
@@ -98,14 +108,71 @@ public class MyPlayer extends Player {
     return best_move;
   }
 
+   private class BestMove{
+      int value;
+      AriesMove move; //TODO: lista ruchów
+
+      BestMove(int value){
+          this.value = value;
+          this.move = new AriesMove(null, 0,0,0,0, null, null);
+      }
+
+       BestMove(int value, Move move){
+           this.value = value;
+           this.move = (AriesMove) move;
+       }
+
+      //TODO: porównanie - przeciążyć operator
+      //TODO: minus unarny = przeciążyć
+  }
+
+  BestMove NegMax(Board board, Color player, int depth){
+      System.out.println("\nNew recursion " + depth + " " + player);
+      int val;
+      if ((val = checkWinning(board, player)) != 0){
+          System.out.println("Win " + player + " "+ val + " on depth " + depth);
+          return new BestMove(val);
+      }
+      if (depth == 0){
+          val = stateEvaluation(board, player);
+          return new BestMove(val);
+      }
+
+      List<Move> moves = board.getMovesFor(player);
+
+      BestMove curr_move;
+      BestMove best_move = new BestMove(Integer.MIN_VALUE+1, moves.get(0));
+      Board boardNew;
+
+
+      for (Move move : moves) {
+          System.out.println(move.toString());
+          boardNew = board.clone();
+          boardNew.doMove(move);
+
+          curr_move = NegMax(boardNew, getOpponent(player), depth - 1);
+          curr_move.value = -curr_move.value;
+
+          if (curr_move.value > best_move.value) {
+              curr_move.move = (AriesMove) move;
+              best_move = curr_move;
+              System.out.println("New best " + player +" " + curr_move.move.toString() + " "+ curr_move.value);
+              if (best_move.value == Integer.MAX_VALUE) break; // winning move
+          }
+          System.out.println("Value for move " + curr_move.move.toString() + " "+ curr_move.value + " for " + player);
+          //TODO: odcięcia
+      }
+      System.out.println("Best move for "+ player + " depth "+ depth  + ": " + best_move.move.toString() + " " + best_move.value);
+      return best_move;
+  }
+
 
   @Override
   public Move nextMove(Board b) {
-    Color player = getColor();
-    System.out.print(player + " ");
+    System.out.println ("NOWY RUCH " + getColor() + "\n-----------------------------");
 
-    List<Move> moves = b.getMovesFor(getColor());
+    BestMove bestMove = NegMax(b, getColor(), MAX_DEPTH);
 
-    return chooseBestMove(moves, b);
+    return bestMove.move;
   }
 }
