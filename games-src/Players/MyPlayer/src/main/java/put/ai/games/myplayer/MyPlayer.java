72312e9c600e8413022acdafe82bc27a14376fe3
panka134 <134panka@gmail.com>
2022-01-23 13:32:15 +0100

    Fix going behind the board -_-

--- games-src/Players/MyPlayer/src/main/java/put/ai/games/myplayer/MyPlayer.java
+++ games-src/Players/MyPlayer/src/main/java/put/ai/games/myplayer/MyPlayer.java
@@ -10,11 +10,15 @@ import java.util.List;
 
 import static java.lang.Math.max;
 import static java.lang.Math.min;
+import static java.lang.Thread.sleep;
 
 public class MyPlayer extends Player {
 
   boolean debug = true;
   int MAX_DEPTH = 2;
+  static int INFTY = Integer.MAX_VALUE;
+  static int LOSE = -INFTY + 1;
+  static int WIN = INFTY - 1;
 
   @Override
   public String getName() {
@@ -29,12 +33,15 @@ public class MyPlayer extends Player {
   }
 
   int checkWinning(Board b, Color player){
-      if (b.getState(0,0) == Color.PLAYER2)
-          if (player == Color.PLAYER2) return Integer.MAX_VALUE;
-          else return Integer.MIN_VALUE+1;
-      else if (b.getState(b.getSize(), b.getSize()) == Color.PLAYER1)
-          if (player == Color.PLAYER1) return Integer.MAX_VALUE;
-          else return Integer.MIN_VALUE+1;
+      System.out.println(b.getState(b.getSize()-1, b.getSize()-1));
+      if (b.getState(0,0) == Color.PLAYER2){
+          if (player == Color.PLAYER2) return WIN;
+          else return LOSE;
+      }
+      else if (b.getState(b.getSize()-1, b.getSize()-1) == Color.PLAYER1){
+          if (player == Color.PLAYER1) return WIN;
+          else return LOSE;
+      }
       else return 0;
   }
 
@@ -46,8 +53,8 @@ public class MyPlayer extends Player {
 
     int my_pieces = 0;
     int opponents_pieces = 0;
-    int my_distance = Integer.MAX_VALUE;
-    int opponents_distance = Integer.MAX_VALUE;
+    int my_distance = INFTY;
+    int opponents_distance = INFTY;
 
     for (int i=0; i<board.getSize(); i++){
       for (int j=0; j<board.getSize(); j++){
@@ -55,24 +62,20 @@ public class MyPlayer extends Player {
               my_pieces++;
               my_distance = min(getDistanceToWinningField(i, j, player, board), my_distance);
               if (my_distance == 0)
-                  return Integer.MAX_VALUE; //our winning move
+                  return WIN; //our winning move
           }
           else if (b.getState(i, j) == opponent){
               opponents_pieces++;
               opponents_distance = min(getDistanceToWinningField(i, j, opponent, board), opponents_distance);
 
               if (opponents_distance == 0)
-                  return Integer.MIN_VALUE+1; //their winning move
+                  return LOSE; //their winning move
           }
       }
     }
 
-//      if (debug){
-//        System.out.println("Pieces: " + player + " " + my_pieces +" "+ opponent + " "+ opponents_pieces);
-//        System.out.println("Distance: " + player + " " +  my_distance + " " + opponent + " " + opponents_distance);
-//      }
-
-      return -9*opponents_pieces - 10*my_distance + 8*my_pieces + 7*opponents_distance; // my_pieces - my_distance - opponents_pieces + opponents_distance;
+      //TODO: Add avg distance, uzależnić wagi od rozmiaru planszy
+      return -9*opponents_pieces - 10*my_distance + 8*my_pieces + 7*opponents_distance;
   }
 
 
@@ -80,7 +83,7 @@ public class MyPlayer extends Player {
     /* Choose move with minimum objective function value */
 
     AriesMove best_move = null;
-    int best_move_value = Integer.MAX_VALUE;
+    int best_move_value = INFTY;
     AriesMove curr_move;
     int curr_move_value;
     Board boardNew;
@@ -96,7 +99,7 @@ public class MyPlayer extends Player {
 //              System.out.println(move.toString() + " " + curr_move_value);
 //          }
 
-          if (curr_move_value == Integer.MIN_VALUE+1) // winning move
+          if (curr_move_value == WIN) // winning move
               return curr_move;
           if (curr_move_value < best_move_value) {
               best_move = curr_move;
@@ -137,8 +140,10 @@ public class MyPlayer extends Player {
           System.out.println("Win " + player + " "+ val + " on depth " + depth);
           return new BestMove(val);
       }
+      else System.out.println("No win " + player + " "+ val + " on depth " + depth);
       if (depth == 0){
           val = stateEvaluation(board, player);
+          System.out.println("return " + val + " (" + player +")");
           return new BestMove(val);
       }
 
@@ -150,13 +155,15 @@ public class MyPlayer extends Player {
       long maxTime = 0;
 
       for (Move move : moves) {
+          System.out.println("Analiza ruchu " + move.toString() +" depth: "+  depth + " " +player);
           if (System.nanoTime() + maxTime > endRoundTime) {
-              System.out.println("Out of time :( " + System.nanoTime() + maxTime + " " + endRoundTime);
+              System.out.println(player + "Out of time :( " + System.nanoTime() + maxTime + " " + endRoundTime);
               //TODO: use bestMove()
               break;  //out of time
           }
 
           start = System.nanoTime();
+
           boardNew = board.clone();
           boardNew.doMove(move);
 
@@ -168,7 +175,10 @@ public class MyPlayer extends Player {
               curr_move.move = (AriesMove) move;
               alpha = curr_move;
               System.out.println("New best " + player +" " + curr_move);
-              if (alpha.value == Integer.MAX_VALUE) break; // winning move
+//              if (alpha.value == WIN) break; // winning move
+          }
+          else{
+              System.out.println("No new best " + player +" " + curr_move + move.toString() + "a: " + alpha);
           }
 
           if (alpha.value >= beta.value) {
@@ -186,7 +196,7 @@ public class MyPlayer extends Player {
 
 
   @Override
-  public Move nextMove(Board b) {
+  public Move nextMove(Board b){
     Color player = getColor();
     System.out.println ("NOWY RUCH " + player + "\n-----------------------------");
 
@@ -195,10 +205,15 @@ public class MyPlayer extends Player {
     BestMove bestMove = NegMax(b,
                                 player,
                                 MAX_DEPTH,
-                                new BestMove(Integer.MIN_VALUE+1),
-                                new BestMove(Integer.MAX_VALUE),
+                                new BestMove(-INFTY),
+                                new BestMove(INFTY),
                                 endRoundTime);
 
-    return bestMove.move;
+//      try {
+//          sleep(3000);
+//      } catch (InterruptedException e) {
+//          e.printStackTrace();
+//      }
+      return bestMove.move;
   }
 }
